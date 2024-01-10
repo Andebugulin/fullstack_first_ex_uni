537fa99fca6436da57ca07d884f129a2735e1b65
Andrei Gulin <gulin.andrey2005@gmail.com>
2024-01-10 19:20:20 +0200

    assignment 1

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-document.getElementById("actionButton").addEventListener("click", function() {
-    alert("Button clicked!");
-});
+// Get current year and update the footer
+const currentYear = new Date().getFullYear();
+document.getElementById('currentYear').innerText = currentYear;
+
+
+// Toggle content function
+function toggleContent() {
+    const paragraph = document.getElementById('description');
+    paragraph.style.display = (paragraph.style.display === 'none' || paragraph.style.display === '') ? 'block' : 'none';
+  }
